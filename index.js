f721dd800564fe3f226cd48acce195bdafe12d13
christian josephe <cjpyane97@gmail.comm>
2024-03-06 09:35:09 -0600

    done

--- index.js
+++ index.js
@@ -1,23 +1,133 @@
+/* eslint-disable func-style */
 
 $(document).ready(() => {
-  const $body = $('body');
+  const $body = $('body');//
   $body.html('');//clears the body
 
-  const $tweetsDiv = $('<div id=tweets>');
-  $body.append($tweetsDiv);
+  const $tweetsDiv = $('<div id=tweets>');//conatainer for holding tweet
+  $body.append($tweetsDiv);// pushes tweets to the body
 
   function MakeNewTweets() {
-  
+  //create func to show new tweets
+    const showTweets = () => {
+      const $tweetsToShow = streams.home.map((tweet) => {
+        const $tweet = $('<div class="tweet"></div>');
+        const text = `<a href="#" class="username" data-user="${tweet.user}">@${tweet.user}</a>: ${tweet.message} (${timestamp(tweet.created_at)})`;
+    
+        $tweet.html(text);
+    
+        return $tweet;
+      });
+      $tweetsDiv.html('');//clear current tweets left
+      $tweetsDiv.append($tweetsToShow);//append new tweets
+      //add username buton
+      $('.username').on('click', function(event) {
+        const user = $(this).data('user');
+        userTimeline(user);
+      });
+    };
+    const $tweetsButton = $('<button>Show New Tweets</button>');//create the tweets button
+    $body.append($tweetsButton);
+    $tweetsButton.on('click', () => {
+      showTweets();//calls whole fun to show new tweets
+    });
+    showTweets(); //first show of tweets
   }
-  const $tweets = streams.home.map((tweet) => {
-    const $tweet = $('<div></div>');
-    const text = `@${tweet.user}: ${tweet.message}`;
+  MakeNewTweets();
 
-    $tweet.text(text);
+  function timestamp(time) {
+    const currTime = new Date();//current time
+    const tweetTime = new Date(time);//make tweets time into usable data
+    const timeDiff = Math.floor((currTime.getTime() - tweetTime.getTime()) / 1000);//use getTime to subtract the diff then turn into seconds instead of milliseconds so readable to user
+    if (timeDiff < 60) {
+      if (timeDiff === 1) {
+        return '1 second ago';
+      } else {
+        return timeDiff + ' seconds ago';
+      }
+    } else if (timeDiff < 3600) {
+      const min = Math.floor(timeDiff / 60); //create minutes data
+      if ( min === 1) {
+        return '1 minute ago';
+      } else {
+        return min + ' minutes ago';
+      }
+    } else if (timeDiff < 86400) {
+      const hrs = Math.floor(timeDiff / 3600);//create hrs data and go thu same if's
+      if (hrs === 1) {
+        return '1 hour ago';
+      } else {
+        return hrs + ' hours ago';
+      }
+    }
+ 
+    
+    
+  }
+  function userTimeline(username) {
+    const userTweetsRec = streams.users[username];
+    const $userTweetShown = userTweetsRec.map((tweet) => {
+      const $tweet = $('<div class="tweet"></div>');
+      const text = `@${tweet.user}: ${tweet.message} (${timestamp(tweet.created_at)})`;
+      $tweet.text(text);
+      return $tweet;
+    });
+    $tweetsDiv.html('');
+    $tweetsDiv.append($userTweetShown);
 
-    return $tweet;
-  });
+
+
+
+  }
+  function userTweet() {
+    const $tweetData = $('<input type = "text" placeholder="Enter your tweet">');
+    const $tweetButton = $('<button>Submit</button>');
+
+    $body.append($tweetData);
+    $body.append($tweetButton);
+
+    $tweetButton.on('click', () => {
+      const tweetMessage = $tweetData.val().trim();
+      if (tweetMessage !== '') {
+        const userTweetObj = {
+          user: 'currentUser',
+          message: tweetMessage,
+          created_at: new Date()
+        };
+        streams.home.push(userTweetObj);
+        MakeNewTweets();
+        $tweetData.val('');
+      }
+     
+    });
+    
+  }
+  userTweet();
   
-  $tweetsDiv.append($tweets);
+
   
+
+
+
+
+
 });
+//timestamp();
+
+
+
+
+
+
+// const $tweets = streams.home.map((tweet) => {
+//   const $tweet = $('<div></div>');
+//   const text = `@${tweet.user}: ${tweet.message}`;
+
+//   $tweet.text(text);
+
+//   return $tweet;
+// });
+  
+//$tweetsDiv.append($tweets);
+  
+//});
